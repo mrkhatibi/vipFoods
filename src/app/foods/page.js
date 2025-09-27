f954c63e850c4801dbaa5a53dd31007eb94e9d28
Mojtaba Khatibi <Khatibi486@gmail.com>
2025-09-27 17:20:44 +0330

    add suspense

--- src/app/foods/page.js
+++ src/app/foods/page.js
@@ -1,57 +1,15 @@
-"use client";
+"use client"
 
-import GetAllFoodsQuery from "@/components/helpers/getAllPostsQuery";
-import PostsCard from "@/components/templates/postsCard";
-import styles from "./Foods.module.css";
-import Link from "next/link";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import Searchfood from "@/components/modules/Searchfood";
-import SearchFood from "@/components/helpers/searchFood";
-import Loader from "@/components/modules/loader/Loader";
+import Loader from "@/components/modules/loader/Loader"
+import FoodsPage from "@/components/templates/FoodsPage"
+import { Suspense } from "react"
+
 function Foods() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const query = searchParams.get("search");
-  const [response, setRes] = useState([]);
-  useEffect(() => {
-    async function getsearch() {
-      const res = await SearchFood(query);
-      setRes(res);
-    }
-    getsearch();
-  }, [query]);
-  const { data, isLoading } = GetAllFoodsQuery();
-  let showData = [];
-  if (isLoading) return <Loader />;
-  {
-    !query ? (showData = data) : (showData = response);
-  }
   return (
-    <div className={styles.allcontainer}>
-      <h3>All Foods</h3>
-      <Searchfood />
-      <div className={styles.deletesearchwraper}>
-
-      {query ? (
-        <button
-          className={styles.deletesearch}
-          onClick={() => router.replace("/foods")}
-        >
-          delete Search
-        </button>
-      ) : null}
-      </div>
-
-      <div className={styles.container}>
-        {showData.map((post) => (
-          <Link href={`/foods/${post._id}`} key={post._id}>
-            <PostsCard post={post} />
-          </Link>
-        ))}
-      </div>
-    </div>
-  );
+    <Suspense fallback={<Loader />}>
+      <FoodsPage />
+    </Suspense>
+  )
 }
 
-export default Foods;
+export default Foods
